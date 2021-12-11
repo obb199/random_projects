20e37818fef7c4ea3a4c2cb2e945d4a474d354b4
Gabriel Carvalho Santana <74666057+obb199@users.noreply.github.com>
2021-12-11 13:38:18 -0300

    Add files via upload

--- calculus/derivative_and_integral_of_a_function.cpp
+++ calculus/derivative_and_integral_of_a_function.cpp
@@ -3,10 +3,19 @@
 
 using namespace std;
 
-double f(double x){
-	return pow(x,3) - x + 5; // f(x) = x³ - x + 5  , F(x) = x**4 / 4 - x² / 2 + 5*x , dy/dx = 3x**2 - 1
-	                                                        
+double f1(double x){
+	return pow(x,3) - x + 5; // f(x) = x³ - x + 5  , F(x) = x**4 / 4 - x² / 2 + 5*x + k , dy/dx = 3x**2 - 1	                                                        
 }
+
+double f2(double x){
+	return x + 3;  // f(x) = x + 3, F(x) = x²/2 + 3x + k, dy/dx = 1
+}
+
+double f3(double x){
+	return cos(x); //f(x) = cos(x), F(x) = sen(x), dy/dx = -sen(x)
+}
+
+
 
 double integral(float a, float b){ //extremuns of integration
 	int sums = (unsigned int)(b-a) * 10000;
@@ -14,7 +23,9 @@ double integral(float a, float b){ //extremuns of integration
 	long double area = 0;
 	
 	for(register double i = a; i < b; i += increase){
-		area += f(i)*(increase);
+		area += abs(f1(i)*(increase));
+		//area += abs(f2(i)*(increase));
+		//area += abs(f3(i)*(increase));
 	}
 	
 	return area;
@@ -22,7 +33,9 @@ double integral(float a, float b){ //extremuns of integration
 
 double derivative(double x){ 
 	double increase = 0.0000001;
-	return (f(x+increase) - f(x))/(increase);
+	return (f1(x+increase) - f1(x))/(increase);
+	//return (f2(x+increase) - f2(x))/(increase);
+	//return (f3(x+increase) - f3(x))/(increase);
 }
 
 int main(){
